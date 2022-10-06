94e41a3abd4fa75e77c65249db25fa4d1c4d1282
Sabahat Hussain <miansabahat10@gmail.com>
2022-10-06 12:58:11 +0500

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,20 +3,20 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
   const [text, setText] = useState('Enter Text here');
 
-  const handleUpClick = () => {
+  const handleUpClick = () => {   
     console.log('Uppercase was clicked');
     let newText = text.toUpperCase();
     setText(newText);
-  }
+  }  
 
-  const handleOnChange = (event) => {
+  const handleOnChange = (event) => {     
     console.log('On Change was clicked');
     setText(event.target.value);
-  }
+  }  
 
-  return (
-    <div>
-        <h1>{props.heading}</h1>
+  return (     
+    <div>      
+        <h1>{props.heading}</h1>      sdfdsf
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
